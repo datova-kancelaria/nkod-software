ded0c54d4f7f57761109bb239169173f8c58bbf0
Petr Škoda <skodapetr@gmail.com>
2022-07-08 11:16:19 +0200

    Update component

--- components/registration-updater/update-registrations.sh
+++ components/registration-updater/update-registrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p /data
-cd /data
-[ ! -d "/data/.git" ] && git clone $WEBHOOK_REPOSITORY ./
+mkdir -p /data/registration
+cd /data/registration
+[ ! -d ".git" ] && git clone $WEBHOOK_REPOSITORY ./
 git fetch --all
 git reset --hard $WEBHOOK_BRANCH
