c0e971a7181dd69a059cc30e6c5e198ef9faec6a
Petr Škoda <skodapetr@gmail.com>
2022-07-08 11:44:01 +0200

    Update scheduler

--- components/scheduler/update-and-run-pipeline.sh
+++ components/scheduler/update-and-run-pipeline.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# This needs to git checkout pipelines. 
-# Apply sed to to put password from values there.
-# Start the first pipeline.
+echo " New run started: $(date)" 
 
-# 01 - Harvestace data.gov.sk - CKAN API
+echo "Checking out data ..."
+mkdir -p /tmp/storage/
+cd /tmp/storage/
+git clone $STORAGE_REPOSITORY ./
 
-echo " $(date): I do not know what to do with this script." >> /data/lp-etl/file.log
+echo "Moving data to storage ..."
+cp -r ./pipelines /data/lp-etl/storage/pipelines
+cp -r ./tmplates /data/lp-etl/storage/tmplates
+
+echo "Removing tmporary data"
+rm -r cd /tmp/storage/
+
+# Execute a POST to given URL to start the execution. 
+# This may actually fail for the first time as the storage
+# do not reload data on demand.
+echo "Executing POST request"
+curl -X POST $POST_URL
