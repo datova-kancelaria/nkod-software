b27c4ea6d1e2797325d4e8af73762e4c734819fd
Petr Škoda <skodapetr@gmail.com>
2022-12-27 21:43:24 +0100

    Update registration
    Update rights on data direcotry.

--- components/registration-updater/registrations-update.sh
+++ components/registration-updater/registrations-update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "Updating repository"
-id -u
-ls -la /data/registration
 
-mkdir -p /data/registration
+# Make sure directory exist with the right settings.
+[ ! -d "/data/registration" ] && mkdir -p /data/registration && chwon nodc:ndoc /data/registration && chmod g+s /data/registration
 
+# Update the content.
 cd /data/registration
 [ ! -d ".git" ] && git clone $WEBHOOK_REPOSITORY ./
 git fetch --all
