309eb7713e6d5d03a281752c4449257d6467150e
Petr Škoda <skodapetr@gmail.com>
2022-12-28 12:23:17 +0100

    Fix running apache

--- components/registration-updater/registrations-entrypoint.sh
+++ components/registration-updater/registrations-entrypoint.sh
@@ -9,12 +9,6 @@
 # Execute initial check.
 /opt/registrations-update.sh
 
-# This is code from php:7.4.30-apache-buster/docker-php-entrypoint
+# This is code based on php:7.4.30-apache-buster/docker-php-entrypoint
 set -e
-
-# First arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-  set -- apache2-foreground "$@"
-fi
-
-exec "$@"
+apache2-foreground
