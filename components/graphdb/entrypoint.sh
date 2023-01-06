7db8468102828b1f8fda18f5ca1a394578b863ae
Petr Škoda <skodapetr@gmail.com>
2023-01-06 14:14:04 +0100

    Update Dockerfiles
    Remove default empty values, add GRAPHDB_EXTERNAL_URL setting

--- components/graphdb/entrypoint.sh
+++ components/graphdb/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Hash passwords.
-GRAPHDB_ADMIN_PASSWORD_BCRYPT=$(htpasswd -bnBC 10 "" $GRAPHDB_ADMIN_PASSWORD | tr -d ':\n')
-GRAPHDB_PASSWORD_BCRYPT=$(htpasswd -bnBC 10 "" $GRAPHDB_PASSWORD | tr -d ':\n')
+ADMIN_PASSWORD_BCRYPT=$(htpasswd -bnBC 10 "" $GRAPHDB_ADMIN_PASSWORD | tr -d ':\n')
+PASSWORD_BCRYPT=$(htpasswd -bnBC 10 "" $GRAPHDB_PASSWORD | tr -d ':\n')
 
 # Prepare configuration
 sed -i \
-  -e "s|{ADMIN_PASSWORD_BCRYPT}|$GRAPHDB_ADMIN_PASSWORD_BCRYPT|g" \
+  -e "s|{ADMIN_PASSWORD_BCRYPT}|$ADMIN_PASSWORD_BCRYPT|g" \
   -e "s|{USERNAME}|$GRAPHDB_USER|g" \
-  -e "s|{PASSWORD_BCRYPT}|$GRAPHDB_PASSWORD_BCRYPT|g" \
+  -e "s|{PASSWORD_BCRYPT}|$PASSWORD_BCRYPT|g" \
   /opt/graphdb/home/work/workbench/settings.js
+
+echo "graphdb.external-url = $GRAPHDB_EXTERNAL_URL\n" >> /opt/graphdb/home/conf/graphdb.properties
 
 # Prepare data directory
 mkdir -p /data/graphdb
